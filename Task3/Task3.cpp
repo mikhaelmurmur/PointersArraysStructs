ea80cc954f4a1c1977b6c6d1c517fb6fa17f7fc7
Mikhaelmurmur <stolovich@yahoo.com>
2016-01-07 16:00:45 +0200

    Task 3 is done.

---
+++ Task3/Task3.cpp
@@ -0,0 +1,19 @@
+// Task3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	typedef unsigned char uChar;
+	typedef const unsigned char cuChar;
+	typedef int* int_p;
+	typedef char** char_p_p;
+	typedef char* char_ar_p;
+	typedef int* sev_int_ar_p[7];
+	typedef sev_int_ar_p* pp;
+	typedef sev_int_ar_p* pp1[8];
+    return 0;
+}
+
